3cf93be3e87bc09a9a093b6fe3ad095ecff9ae02
Mohamed Sayed <msby1144@gmail.com>
2025-07-09 21:51:42 +0300

    Add service package with MailService and ShippingService

---
+++ src/service/MailService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class MailService {
+    public static void sendMail(String email) {
+        System.out.println("Book store send to  => " + email);
+    }
+}
---
+++ src/service/ShippingService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class ShippingService {
+    public static void ship(String address) {
+        System.out.println("Book store shipping to address => " + address);
+    }
+}
